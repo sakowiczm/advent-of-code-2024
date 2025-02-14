5a63a87d510224cbf68b9f837038bc3d2572cd13
Michal Sakowicz <sakowiczm@gmail.com>
2025-02-14 16:36:32 +0000

    Day 16 Part 2

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,27 +1,23 @@
 ﻿// https://adventofcode.com/2024/day/16
 
-var map = File.ReadAllLines("input.txt")
+var map = File.ReadAllLines("test1.txt")
     .Select(o => o.ToCharArray())
     .ToArray();
 
 var maze = new Maze(map);
-//maze.Print();
 
-Console.WriteLine(maze.FindCheapestPath());
+var (cost, steps) = maze.GetPath();
 
-// test1.txt -> 7036
-// test2.txt -> 11048
+Console.WriteLine($"Cost: {cost}");
+Console.WriteLine($"Seats: {steps.Count}");
+
+//maze.PrintPath(steps);
 
 public record Point(int Row, int Col)
 {
     public static Point operator +(Point a, Direction b)
     {
         return new Point(a.Row + b.Row, a.Col + b.Col);
-    }
-    
-    public static Point operator -(Point a, Direction b)
-    {
-        return new Point(a.Row - b.Row, a.Col - b.Col);
     }
 }
 
@@ -46,13 +42,14 @@ public static class DirectionExtensions
     public static Direction Rotate(this Direction direction, Rotation rotation) 
         => rotation switch
         {
-            Rotation.Right => new Direction(direction.Col, -direction.Row),
-            Rotation.Left => new Direction(-direction.Col, direction.Row),
+            Rotation.Right => new Direction(-direction.Col, direction.Row),
+            Rotation.Left => new Direction(direction.Col, -direction.Row),
             _ => throw new InvalidOperationException("Invalid rotation value.")
         };
 }
 
 public record Position(Point Location, Direction Direction);
+public record Path(int Cost, List<Point> Steps);
 
 public class Maze(char[][] map)
 {
@@ -61,9 +58,10 @@ public class Maze(char[][] map)
     public char[][] Map { get; } = map;
     public char GetValue(Point pos) => Map[pos.Row][pos.Col];
     public bool IsWall(Point pos) => GetValue(pos) == '#';
-    public bool IsBound(Point pos) => pos.Row >= 0 && pos.Row <= Rows && pos.Col >= 0 && pos.Col <= Cols;
+    public bool IsMaze(Point pos) => pos.Row >= 0 && pos.Row <= Rows && pos.Col >= 0 && pos.Col <= Cols;
     public Point GetStart() => GetPoint('S');
     public Point GetFinish() => GetPoint('E');
+    public bool IsFinish(Point p) => p == GetFinish();
 
     private Point GetPoint(char target)
     {
@@ -81,48 +79,76 @@ public class Maze(char[][] map)
         throw new InvalidOperationException($"No point '{target}' found in the maze.");
     }
 
-    public int FindCheapestPath()
+    public (int Cost, List<Point> Steps) GetPath()
     {
-        var startPosition = new Position(GetStart(), Direction.East);
-
-        var visitedPositions = new HashSet<Position>();
-        var positions = new PriorityQueue<Position, int>([(startPosition, 0)]);
-        var minPositionCost = new Dictionary<Position, int> { [startPosition] = 0 };
+        var start = new Position(GetStart(), Direction.East);
+        var visited = new HashSet<Position>();
+        var positions = new PriorityQueue<Position, int>([(start, 0)]);
+        var paths = new Dictionary<Position, Path> { [start] = new(0, []) };
 
         while (positions.Count > 0)
         {
-            var currentPos = positions.Dequeue();
-            var currentCost = minPositionCost[currentPos];
-            visitedPositions.Add(currentPos);
+            var current = positions.Dequeue();
             
-            var moves = new[]
+            if(IsFinish(current.Location))
             {
-                (currentPos with { Location = currentPos.Location + currentPos.Direction }, currentCost + 1),
-                (currentPos with { Direction = currentPos.Direction.Rotate(Rotation.Right) }, currentCost + 1000),
-                (currentPos with { Direction = currentPos.Direction.Rotate(Rotation.Left) }, currentCost + 1000)
+                var (cost, steps) = paths[current];
+                return (cost, steps.Distinct().ToList());
+            }
+            
+            if(!visited.Add(current))
+                continue;
+
+            var path = paths[current];
+            
+            var nextMoves = new[]
+            {
+                (current with { Location = current.Location + current.Direction }, path.Cost + 1),
+                (current with { Direction = current.Direction.Rotate(Rotation.Right) }, path.Cost + 1000),
+                (current with { Direction = current.Direction.Rotate(Rotation.Left) }, path.Cost + 1000)
             };
 
-            foreach (var (pos, cost) in moves)
+            foreach (var (nextPos, nextCost) in nextMoves)
             {
-                if (!IsBound(pos.Location) || IsWall(pos.Location) || visitedPositions.Contains(pos) || (minPositionCost.TryGetValue(pos, out var positionCost) && positionCost <= cost))
+                if (!IsMaze(nextPos.Location) || IsWall(nextPos.Location))
                     continue;
 
-                minPositionCost[pos] = cost;
-                positions.Enqueue(pos, cost);
+                // cost higher move on
+                if (paths.TryGetValue(nextPos, out var posPath) && nextCost > posPath.Cost)
+                    continue;
+                
+                // no cost or less
+                if (posPath == null || nextCost < posPath.Cost)
+                {
+                    paths[nextPos] = new Path(nextCost, [..path.Steps, nextPos.Location]);
+                }
+                else
+                {
+                    // cost is equal
+                    paths[nextPos] = new Path(nextCost, [..path.Steps, ..posPath.Steps, nextPos.Location]);;
+                }
+
+                positions.Enqueue(nextPos, nextCost);
             }
         }
 
-        return Direction.All.Select(d => new Position(GetFinish(), d))
-            .Where(p => minPositionCost.ContainsKey(p))
-            .Select(p => minPositionCost[p])
-            .Min();
+        return (-1, []);
     }
 
-    public void Print()
+    public void PrintPath(IEnumerable<Point> path)
     {
-        foreach (var row in Map)
+        var copy = Map.Select(row => row.ToArray()).ToArray();
+
+        foreach (var point in path)
+        {
+            copy[point.Row][point.Col] = '0';
+        }
+        
+        Console.WriteLine();
+        
+        foreach (var row in copy)
         {
             Console.WriteLine(row);
-        }
-    }
+        }        
+    } 
 }
